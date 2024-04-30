0101a137ee402abf7f1050854bdcca34f7c3a011
Никита <nikita0112358@yandex.ru>
2024-04-30 19:48:49 +0500

    Начало

--- config/global_config.py
+++ config/global_config.py
@@ -1,2 +1,2 @@
 API_KEY = 'dcdc9a5e10f14ba092d141240243004'
-URL = 'http://api.weatherapi.com/v1'
+URL = 'http://api.weatherapi.com/v1/current.json'
---
+++ main.py
@@ -0,0 +1,11 @@
+print(132)
+
+import requests
+from config.global_config import *
+
+api_key = API_KEY
+
+url = URL
+
+
+
