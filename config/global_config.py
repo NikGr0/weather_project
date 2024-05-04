38e5934b98de5c25f15758bb40526700d61e8f4e
Никита <nikita0112358@yandex.ru>
2024-05-04 11:33:45 +0500

    Comment

--- config/global_config.py
+++ config/global_config.py
@@ -1,2 +1,4 @@
 API_KEY = 'dcdc9a5e10f14ba092d141240243004'
-URL = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=51.7727,55.0988&country=Russia'
+URL = f'http://api.weatherapi.com/v1/current.json?key='
+
+# f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=51.7727,55.0988'
--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-
+import time
 
 import requests
+import yaml
+import datetime
+import pandas as pd
 from config.global_config import *
+import config as con
 
-#api_key = API_KEY
+# with open(r'config\list_cities.yaml', 'w') as file:
+#     doc = yaml.dump(['51.7727,55.0988', '', '59.9386,30.3141', '56.8519,60.6122', '55.0415,82.9346'], file)
 
-#URL = URL
+# читаем список городов
+with open(r'config\list_cities.yaml') as file:
+    list_cities = yaml.load(file, Loader=yaml.FullLoader)
+print(list_cities)
 
-r = requests.get(url=URL)
-result = r.json()
-print(result)
+# получаем название файла csv
+dt1 = datetime.datetime.now()
+dt_now = 'history/' + str(dt1)[:-7]
+print(dt_now)
+# получаем данные
+weather = []
+for i in list_cities:
+    r = requests.get(url=URL + f'{API_KEY}&q={i}')
+    result = r.json()
 
+    city = result.get('location').get('name')
+    temp = result.get('current').get('temp_c')
+    cloud = result.get('current').get('cloud')
 
+    d = {'Город': city,
+         'Температура, °С': temp,
+         'Облачность, %': cloud
+         }
+    weather.append(d)
+    time.sleep(1)
+#print(d)
+print(weather)
+
+from_history = pd.DataFrame(weather)
+from_history.to_csv(dt_now)
