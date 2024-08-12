9c5b72ed77110540b67dd67b082b9c303ab64a0e
Nik <nikitagr01@gmail.com>
2024-08-12 19:55:47 +0500

    Начал создавать проект для записи в БД
    Signed-off-by: Nik <nikitagr01@gmail.com>

--- config/global_config.py
+++ config/global_config.py
@@ -1,4 +1,5 @@
 API_KEY = 'dcdc9a5e10f14ba092d141240243004'
 URL = f'http://api.weatherapi.com/v1/current.json?key='
 
+SQLALCHEMY_DATABASE_URI = f"postgresql://nik:675756@192.168.9.130:5432/test1"
 # f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=51.7727,55.0988'
---
+++ whrite_to_db.py
@@ -0,0 +1,36 @@
+
+import time
+import requests
+import yaml
+from config.global_config import *
+import datetime as datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+
+
+
+
+# читаем список городов
+with open(r'config\list_cities.yaml') as file:
+    list_cities = yaml.load(file, Loader=yaml.FullLoader)
+
+
+# получаем данные
+weather = []
+for i in list_cities:
+    r = requests.get(url=URL + f'{API_KEY}&q={i}')
+    result = r.json()
+
+    city = result.get('location').get('name')
+    temp = result.get('current').get('temp_c')
+    cloud = result.get('current').get('cloud')
+
+    d = {'Город': city,
+         'Температура, °С': temp,
+         'Облачность, %': cloud
+         }
+    weather.append(d)
+    time.sleep(1)
+
